4f1293bca51fe63551a67c9e04d0697a483dd924
air <kovalenkokv@gmail.com>
2023-01-31 00:34:32 +0300

    complete homework

--- regexp_homework.py
+++ regexp_homework.py
@@ -16,15 +16,53 @@ lastname,firstname,surname,organization,position,phone,email
 '''
 
 from pprint import pprint
-# читаем адресную книгу в формате CSV в список contacts_list
+import re
 import csv
 with open("phonebook_raw.csv") as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
 pprint(contacts_list)
-
 # TODO 1: выполните пункты 1-3 ДЗ
 # ваш код
+export = []
+for row in contacts_list:
+  if row[0] == 'lastname':
+    export.append(row)
+  else:
+    #parce FIO
+    for c  in range(3):
+      fio = re.split('\s', row[c])
+      if fio !=None or len(fio) >1 :
+        for i in range(len(fio)):
+          row[i+c] = fio[i]
+    #parce Phone
+    if row[5] != '':
+      tel_split = re.split('доб.',row[5])
+      phone = re.findall("\d",tel_split[0])
+      row[5] = f'+7({"".join(phone[1:4])}){"".join(phone[4:7])}-{"".join(phone[7:9])}-{"".join(phone[9:11])}'
+      if len(tel_split) > 1:
+        phone = re.findall("\d", tel_split[1])
+        row[5]+= f" доб.{''.join(phone)};"
+    #find duplicates
+    counter = 0
+    #Сравниваем по ФИО если больше 2х совпадений то считаем что дубликат
+    if len(export) != 0:
+      # Если выходной список не пуст, бежим по всем записям
+      for c in range(len(export)):
+        #Сравниваем ФИО и счетаем совпадения полей
+        for i in range(3):
+          if export[c][i] == row[i]:
+            counter +=1
+      #Если их больше 1 то считаем что дубликат
+      if counter > 1:
+        for i in range(7):
+          # пробегаемся по всем полям и если в выходном списке поле пустое а обработанном поле заполненно, обновляем.
+          export[c][i]  = row[i] if export[c][i] == '' and row[i] != '' else export[c][i]
+      else:
+        #Если совпадений не найдено, добавляем новую запись
+        export.append(row)
+contacts_list = export
+pprint(contacts_list)
 
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
